b2fafc074820806afa0bba30aff38eadd0a7e7e0
chenshuo <c1094829085@qq.com>
2025-04-10 22:16:58 +0200

    the entrance script

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
+
+# make sure to server using VLLM
 python scripts/run_web_thinker_report_brave_search.py \
-    --single_question "What is the capital of France?" \
-    --api_base_url "http://localhost:8000" \
-    --model_name "QwQ-32B"
+    --single_question "What are the models of OpenAI and what are the differences?" \
+    --api_base_url "http://localhost:8000/v1" \
+    --model_name "Qwen/QwQ-32B" \
+    --aux_api_base_url "http://localhost:8000/v1" \
+    --aux_model_name "Qwen/QwQ-32B"
