4484d586718dd3cc948834db1096c77779faadbc
Shuo Chen <chenss@amazon.de>
2025-04-10 17:43:12 +0200

    add entrance script

---
+++ run.sh
@@ -0,0 +1,4 @@
+python scripts/run_web_thinker_report_brave_search.py \
+    --single_question "What is the capital of France?" \
+    --api_base_url "http://localhost:8000" \
+    --model_name "QwQ-32B"
