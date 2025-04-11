2bfd9f71b0d37dee8d5e029361ad7fbf3fcc529e
Zonggen Li <zonggenli@gmail.com>
2025-04-12 00:12:22 +0200

    first commit

--- run.sh
+++ run.sh
@@ -1,7 +1,16 @@
+#!/bin/bash
 
-# make sure to server using VLLM
+# python scripts/run_web_thinker_report_brave_search.py \
+#   --single_question "What are the models of OpenAI and what are the differences?" \
+#   --api_base_url "http://localhost:8000/v1" \
+#   --model_name "Qwen/QwQ-32B" \
+#   --aux_api_base_url "http://localhost:8000/v1" \
+#   --aux_model_name "Qwen/QwQ-32B"
+
 python scripts/run_web_thinker_report_brave_search.py \
-    --single_question "What are the models of OpenAI and what are the differences?" \
+    --dataset_name gaia \
+    --split test \
+    --concurrent_limit 32 \
     --api_base_url "http://localhost:8000/v1" \
     --model_name "Qwen/QwQ-32B" \
     --aux_api_base_url "http://localhost:8000/v1" \
