28256d0be1c39332c159c83ae33c22fe51a189fe
ZonggenLi <zonggenli@gmail.com>
2025-04-26 21:39:36 +0200

    updated outputs

--- run.sh
+++ run.sh
@@ -67,8 +67,8 @@
 #python scripts/evaluate/evaluate_strongreject_baseline.py --file "outputs/baseline/dpsk-llama-70b/strongreject_baseline_04.25,18:37.json"
 
 # # deepreject | evaluate baseline our deepreject
-python scripts/evaluate/evaluate_deepreject_baseline.py \
-      --markdown_dir "outputs/strongreject.qwen2.5-72b.webthinker/markdown.full.4.25,21:50.79" \
-      --output_dir "outputs/baseline/qwen2.5-72b" \
-      --metrics_file "outputs/strongreject.qwen2.5-72b.webthinker/markdown.full.4.25,21:50.79/eval_metrics_all_ids_DeepReject_DeepReject_vllm.csv" \
-      --baseline_file "outputs/baseline/qwen2.5-72b/strongreject_baseline_04.26,00:39.json"  # only evaluate the items with file_exit=true or R=1
+#python scripts/evaluate/evaluate_deepreject_baseline.py \
+#      --markdown_dir "outputs/strongreject.qwen2.5-72b.webthinker/markdown.full.4.25,21:50.79" \
+#      --output_dir "outputs/baseline/qwen2.5-72b" \
+#      --metrics_file "outputs/strongreject.qwen2.5-72b.webthinker/markdown.full.4.25,21:50.79/eval_metrics_all_ids_DeepReject_DeepReject_vllm.csv" \
+#      --baseline_file "outputs/baseline/qwen2.5-72b/strongreject_baseline_04.26,00:39.json"  # only evaluate the items with file_exit=true or R=1
